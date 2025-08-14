6884ffcf919e609c892f0f7245326cba57148b12
m.kazzazi <m.kazzazi@abantether.com>
2025-08-14 17:13:46 +0330

    Add new url and view for mySite app

--- website/views.py
+++ website/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+def http_test(request):
+    return HttpResponse("<h1>salam</h1>")
+
+def json_test(request):
+    return JsonResponse({'hello': 'world'})
