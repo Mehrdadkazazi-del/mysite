f9443a7c3604921ef6651b77531d179678a117f3
Mehrdadkazazi-del <mehrdad.kazazi70@gmail.com>
2025-08-14 19:50:42 +0330

    Added templates for home page, contact page and about page

--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
 
 def index_view(request):
-    return HttpResponse("<h1>Home page</h1>")
+    return render(request, 'website/index.html')
+
 
 def about_view(request):
-    return HttpResponse({'<h1>About page</h1>'})
+    return render(request, 'website/about.html')
+
 
 def contact_view(request):
-    return HttpResponse({'<h1>Contact page</h1>'})
+    return render(request, 'website/contact.html')
