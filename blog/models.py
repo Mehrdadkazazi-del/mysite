a624379e1605b757e5ce9aa4993715d3de394152
Mehrdadkazazi-del <mehrdad.kazazi70@gmail.com>
2025-08-19 00:39:54 +0330

    Added models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    #image
+    #author
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    #tag
+    #category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
